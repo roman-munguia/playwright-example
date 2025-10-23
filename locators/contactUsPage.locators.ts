cda3be62b74d7612d3fc3e3d31598f35a5039af9
Berenice Villa <berenice.villa@unosquare.com>
2025-10-22 18:31:09 -0600

    Complete fill contact form method

--- locators/contactUsPage.locators.ts
+++ locators/contactUsPage.locators.ts
@@ -5,4 +5,8 @@ export const contactUsPageLocators: LocatorObj = {
 
   // CSS Selector Locator
   firstNameInput: 'input[name="firstname"]',
+  lastNameInput: 'input[name="lastname"]',
+  emailInput:  'input[name="email"]',
+  messageInput: 'id="message-bf41a508-ab94-4634-91d7-ace57e01d492"',
+  unlockDedicatedTalentInput: 'input[type="submit"]'
 };
--- pages/contactUsPage.page.ts
+++ pages/contactUsPage.page.ts
@@ -6,10 +6,18 @@ import { contactUsPageLocators } from '../locators/contactUsPage.locators';
 
 export class ContactUsPage extends BasePage {
   readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly emailInput: Locator;
+  readonly messageInput: Locator;
+  readonly unlockDedicatedTalentInput: Locator;
 
   constructor(page: Page) {
     super(page);
     this.firstNameInput = page.locator(contactUsPageLocators.firstNameInput);
+    this.lastNameInput = page.locator(contactUsPageLocators.lastNameInput);
+    this.emailInput = page.locator(contactUsPageLocators.emailInput);
+    this.messageInput = page.locator(contactUsPageLocators.emailInput);
+    this.unlockDedicatedTalentInput = page.locator(contactUsPageLocators.unlockDedicatedTalentInput);
   }
 
   async goTo(): Promise<void> {
@@ -22,5 +30,9 @@ export class ContactUsPage extends BasePage {
 
   async fillContactForm(): Promise<void> {
     await this.firstNameInput.fill('Roman');
+    await this.lastNameInput.fill('Munguia');
+    await this.emailInput.fill('test@unosquare.com');
+    await this.messageInput.fill('This is the SDET course by Roman Munguia.');
+    await this.unlockDedicatedTalentInput.hover();
   }
 }
